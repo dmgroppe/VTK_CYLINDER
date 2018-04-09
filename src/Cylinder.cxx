cb8ad103eeab081d1a1edbff5eac6f9683021811
dmgroppe <dgroppe@cogsci.ucsd.edu>
2018-04-09 13:17:26 -0400

    Command line color control added

--- src/Cylinder.cxx
+++ src/Cylinder.cxx
@@ -1,21 +1,3 @@
-/*=========================================================================
-
-  Program:   Visualization Toolkit
-  Module:    Cylinder.cxx
-
-  Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen
-  All rights reserved.
-  See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
-
-     This software is distributed WITHOUT ANY WARRANTY; without even
-     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
-     PURPOSE.  See the above copyright notice for more information.
-
-=========================================================================*/
-//
-// This simple example shows how to do basic rendering and pipeline
-// creation using C++.
-//
 #include "vtkCylinderSource.h"
 #include "vtkPolyDataMapper.h"
 #include "vtkActor.h"
@@ -24,68 +6,81 @@
 #include "vtkRenderWindowInteractor.h"
 #include "vtkProperty.h"
 #include "vtkCamera.h"
+#include "vtkSmartPointer.h"
+#include <sstream>
 
-int main()
+
+int main(int argc, char *argv[])
 {
-  // This creates a polygonal cylinder model with eight circumferential facets.
-  //
-  vtkCylinderSource *cylinder = vtkCylinderSource::New();
+  // Check args
+  if ((argc<= 1) || (argc>2))
+  {
+	std::cout << "Usage: Cylinder.exe x (where -1<=x<=1)\n";
+	exit(1);
+  }
+  // Convert command line input into a hypothetical voltage value
+  std::stringstream convert(argv[1]);
+  float voltage;
+  if (!(convert >> voltage))
+	  voltage = 0; // Default value for voltage if conversion fails
+  // Cap voltage values
+  if (voltage > 1) voltage = 1;
+  if (voltage < -1) voltage = -1;
+
+  // This creates a polygonal cylinder model with eight circumferential facets
+  // (i.e, in practice an octagonal prism).
+  vtkSmartPointer<vtkCylinderSource> cylinder =
+    vtkSmartPointer<vtkCylinderSource>::New();
   cylinder->SetResolution(8);
 
-  // The mapper is responsible for pushing the geometry into the graphics
-  // library. It may also do color mapping, if scalars or other attributes
-  // are defined.
-  //
-  vtkPolyDataMapper *cylinderMapper = vtkPolyDataMapper::New();
+  // The mapper is responsible for pushing the geometry into the graphics library.
+  // It may also do color mapping, if scalars or other attributes are defined.
+  vtkSmartPointer<vtkPolyDataMapper> cylinderMapper =
+    vtkSmartPointer<vtkPolyDataMapper>::New();
   cylinderMapper->SetInputConnection(cylinder->GetOutputPort());
 
   // The actor is a grouping mechanism: besides the geometry (mapper), it
   // also has a property, transformation matrix, and/or texture map.
-  // Here we set its color and rotate it -22.5 degrees.
-  vtkActor *cylinderActor = vtkActor::New();
+  // Here we set its color and rotate it around the X and Y axes.
+  vtkSmartPointer<vtkActor> cylinderActor =
+    vtkSmartPointer<vtkActor>::New();
   cylinderActor->SetMapper(cylinderMapper);
-  cylinderActor->GetProperty()->SetColor(1.0000, 0.3882, 0.2784);
-  cylinderActor->RotateX(30.0);
+  // Convert voltage value to red-blue color scale (0=white)
+  if (voltage >= 0)
+    cylinderActor->GetProperty()->SetColor(1, (1.0 - voltage), (1.0 - voltage));
+  else
+	cylinderActor->GetProperty()->SetColor((1.0 + voltage), (1.0 + voltage), 1);
+  // These set the initial orientation of the cylinder
+  cylinderActor->RotateX(30.0); 
   cylinderActor->RotateY(-45.0);
 
-  // Create the graphics structure. The renderer renders into the
-  // render window. The render window interactor captures mouse events
+  // The renderer generates the image
+  // which is then displayed on the render window.
+  // It can be thought of as a scene to which the actor is added
+  vtkSmartPointer<vtkRenderer> renderer =
+    vtkSmartPointer<vtkRenderer>::New();
+  renderer->AddActor(cylinderActor);
+  renderer->SetBackground(0.1, 0.2, 0.4);
+  // Zoom in a little by accessing the camera and invoking its "Zoom" method.
+  renderer->ResetCamera();
+  renderer->GetActiveCamera()->Zoom(1.5);
+
+  // The render window is the actual GUI window
+  // that appears on the computer screen
+  vtkSmartPointer<vtkRenderWindow> renderWindow =
+    vtkSmartPointer<vtkRenderWindow>::New();
+  renderWindow->SetSize(200, 200);
+  renderWindow->AddRenderer(renderer);
+
+  // The render window interactor captures mouse events
   // and will perform appropriate camera or actor manipulation
   // depending on the nature of the events.
-  //
-  vtkRenderer *ren1 = vtkRenderer::New();
-  vtkRenderWindow *renWin = vtkRenderWindow::New();
-  renWin->AddRenderer(ren1);
-  vtkRenderWindowInteractor *iren = vtkRenderWindowInteractor::New();
-  iren->SetRenderWindow(renWin);
-
-  // Add the actors to the renderer, set the background and size
-  //
-  ren1->AddActor(cylinderActor);
-  ren1->SetBackground(0.1, 0.2, 0.4);
-  renWin->SetSize(200, 200);
-
-  // We'll zoom in a little by accessing the camera and invoking a "Zoom"
-  // method on it.
-  ren1->ResetCamera();
-  ren1->GetActiveCamera()->Zoom(1.5);
-  renWin->Render();
+  vtkSmartPointer<vtkRenderWindowInteractor> renderWindowInteractor =
+    vtkSmartPointer<vtkRenderWindowInteractor>::New();
+  renderWindowInteractor->SetRenderWindow(renderWindow);
 
   // This starts the event loop and as a side effect causes an initial render.
-  iren->Start();
+  renderWindowInteractor->Start();
 
-  // Exiting from here, we have to delete all the instances that
-  // have been created.
-  cylinder->Delete();
-  cylinderMapper->Delete();
-  cylinderActor->Delete();
-  ren1->Delete();
-  renWin->Delete();
-  iren->Delete();
-
-  return 0;
+  return EXIT_SUCCESS;
 }
-
-
-
-
